d2693701cf5ede12a7308375d44455c1c3166626
Baron Leonardo <mohamedayman.fcis@zoho.com>
2017-03-08 00:07:57 +0200

    replace `assertEqual` by `assertTrue` for readability

--- MainAlgorithms/Ceaser/CeaserTest.py
+++ MainAlgorithms/Ceaser/CeaserTest.py
@@ -21,27 +21,27 @@ class CeaserTest(unittest.TestCase):
     def test_CeaserTestEnc1(self):
         algorithm = Ceaser()
         cipher = algorithm.encrypt(self.mainPlain1, self.mainKey1)
-        self.assertTrue(cipher == self.mainCipher1)
+        self.assertTrue(cipher, self.mainCipher1)
 
     def test_CeaserTestDec1(self):
         algorithm = Ceaser()
         plain = algorithm.decrypt(self.mainCipher1, self.mainKey1)
-        self.assertTrue(plain == self.mainPlain1)
+        self.assertTrue(plain, self.mainPlain1)
 
     def test_CeaserTestAnalysis1(self):
         algorithm = Ceaser()
         key = algorithm.analyse(self.mainPlain1 ,self.mainCipher1)
-        self.assertEqual(self.mainKey1, key)
+        self.assertTrue(self.mainKey1, key)
 
     def test_CeaserTestEnc2(self):
         algorithm = Ceaser()
         cipher = algorithm.encrypt(self.mainPlain2, self.mainKey2)
-        self.assertTrue(cipher == self.mainCipher2)
+        self.assertTrue(cipher, self.mainCipher2)
 
     def test_CeaserTestDec2(self):
         algorithm = Ceaser()
         plain = algorithm.decrypt(self.mainCipher2, self.mainKey2)
-        self.assertTrue(plain == self.mainPlain2)
+        self.assertTrue(plain, self.mainPlain2)
 
     def test_CeaserTestAnalysis2(self):
         algorithm = Ceaser()
@@ -51,32 +51,32 @@ class CeaserTest(unittest.TestCase):
     def test_CeaserTestEnc3(self):
         algorithm = Ceaser()
         cipher = algorithm.encrypt(self.mainPlain3, self.mainKey3)
-        self.assertTrue(cipher == self.mainCipher3)
+        self.assertTrue(cipher, self.mainCipher3)
 
     def test_CeaserTestDec3(self):
         algorithm = Ceaser()
         plain = algorithm.decrypt(self.mainCipher3, self.mainKey3)
-        self.assertTrue(plain == self.mainPlain2)
+        self.assertTrue(plain, self.mainPlain2)
 
     def test_CeaserTestAnalysis3(self):
         algorithm = Ceaser()
         key = algorithm.analyse(self.mainPlain3 ,self.mainCipher3)
-        self.assertEqual(self.mainKey3, key)
+        self.assertTrue(self.mainKey3, key)
 
     def test_CeaserTestNewEnc1(self):
         algorithm = Ceaser()
         cipher = algorithm.encrypt(self.newPlain, self.newKey)
-        self.assertTrue(cipher == self.newCipher)
+        self.assertTrue(cipher, self.newCipher)
 
     def test_CeaserTestNewDec1(self):
         algorithm = Ceaser()
         plain = algorithm.decrypt(self.newCipher, self.newKey)
-        self.assertTrue(plain == self.mainPlain)
+        self.assertTrue(plain, self.newPlain)
 
     def test_CeaserTestNewAnalysis1(self):
         algorithm = Ceaser()
         key = algorithm.analyse(self.newPlain,self.newCipher)
-        self.assertEqual(self.newKey, key)
+        self.assertTrue(self.newKey, key)
 
 
 if __name__ == '__main__':
