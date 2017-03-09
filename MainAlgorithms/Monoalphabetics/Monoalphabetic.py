693d9176582ac9635686ff3ae347bf4dcb4a29cb
Mahmoud Anwer <mahmoudanwer071@gmail.com>
2017-03-09 23:50:09 +0200

    Update

--- MainAlgorithms/Monoalphabetics/Monoalphabetic.py
+++ MainAlgorithms/Monoalphabetics/Monoalphabetic.py
@@ -1,4 +1,4 @@
-﻿
+
 
 class Monoalphabetic:
     """ Frequency Information:
@@ -31,14 +31,83 @@ class Monoalphabetic:
     """
 
     def analyse(self, plainText: str, cipherText: str) -> str:
-        raise NotImplementedError
+        LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        mainKey=""
+        temp = plainText.upper()
+        i = 0
+        while i < len(plainText):
+            index = LETTERS.index(temp[i])
+            mainKey = mainKey[:index] + cipherText[i] + mainKey[index+1:]
+            i = i + 1
+        temp = mainKey
+        temp = temp.upper()
+        i = 0
+        while i < len(temp):
+            if temp[i] == '-':
+                index = LETTERS.index(temp[i - 1])
+                if index == 25:
+                    index = -1
+                temp = temp[:i] + LETTERS[index + 1] + temp[i + 1:]
+            i = i + 1
+        temp = temp.lower()
+        return temp
 
     def decrypt(self, cipherText: str, key: str) -> str:
-        raise NotImplementedError
+        LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        temp = cipherText.upper()
+        KEY = key.upper()
+        plain = ""
+        i = 0
+        while i < len(cipherText):
+            index = KEY.index(temp[i])
+            plain += LETTERS[index]
+            i = i + 1
+        plain = plain.lower()
+        return plain
+
 
     def encrypt(self, plainText: str, key: str) -> str:
-        raise NotImplementedError
+        LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        temp = plainText.upper()
+        i = 0
+        EncryptedText = ""
+        while i < len(plainText):
+            index = LETTERS.index(temp[i])
+            EncryptedText += key[index]
+            i = i + 1
+            EncryptedText = EncryptedText.upper()
+        return EncryptedText
 
     def analyseUsingCharFrequency(self,  cipher: str) -> str:
-        raise NotImplementedError
+        freqInfo = "ETAOINSRHLDCUMFPGWYBVKXJQZ"
+        newTemp = "-" * len(cipher)
+        temp = cipher.upper()
+        dictionary = {}
+        for letters in temp:
+            dictionary[letters] = 0
+        for letters in temp:
+            dictionary[letters] += 1
+        dictionary = sorted(dictionary.items(), reverse=True, key=lambda x: x[1])
+        #print("len: ", len(temp))
+
+        for position in range(0, len(temp)):
+            #print("position: ", position)
+            #print(dictionary[position])
+            if position >= len(dictionary) - 1:
+                break
+        #print("dict: ", dictionary[1][0])
+        i = 0
+        while i < len(dictionary):
+            #print(len(dictionary))
+            #print(dictionary[i][0])
+            j = 0
+            while j < len(temp):
+                #print("temp: ", temp[j],"dict: ", dictionary[i][0])
+                if temp[j] == dictionary[i][0]:
+                    #print("..", temp[j:])
+                    newTemp = newTemp[:j] + freqInfo[i] + newTemp[j + 1:]
+                    #print("tmp: ", temp)
+                j = j + 1
+            i = i + 1
+        return newTemp
 
